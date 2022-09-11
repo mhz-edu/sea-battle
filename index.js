75fb7381d13897daf3ccd43aa207d9a7e3002896
unknown <mhz.edu@gmail.com>
2022-09-11 23:24:50 +0300

    Added model and controller classes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const size = 3;
 
 class View {
-  constructor(model, rootElement, playerShootCallback) {
+  constructor(model, rootElement, controller) {
     this.model = model;
     this.root = rootElement;
-    this.playerShootCallback = playerShootCallback;
+    this.controller = controller;
   }
 
   displayOwnField() {
@@ -14,7 +14,7 @@ class View {
       const row = document.createElement('tr');
       for (let j = 0; j < size; j++) {
         const cell = document.createElement('td');
-        cell.innerText = this.model.ownField[j][i];
+        cell.innerText = this.model.ownField[i][j];
         row.appendChild(cell);
       }
       table.appendChild(row);
@@ -29,13 +29,13 @@ class View {
       const row = document.createElement('tr');
       for (let j = 0; j < size; j++) {
         const cell = document.createElement('td');
-        cell.innerText = this.model.enemyField[j][i];
+        cell.innerText = this.model.enemyField[i][j];
         cell.classList.add('player');
         cell.setAttribute('data-value', `${j}${i}`);
         cell.addEventListener('click', (event) => {
           console.log(event.target.dataset.value);
           const [x, y] = event.target.dataset.value.split('');
-          this.playerShootCallback(parseInt(x), parseInt(y));
+          this.controller.shoot(parseInt(x), parseInt(y));
         });
         row.appendChild(cell);
       }
@@ -50,68 +50,119 @@ class View {
   }
 
   updateOwnField() {
-    const container1 = this.root.querySelector('#player1-container');
-    this.container1.remove();
+    const field = this.root.querySelector('#player1-container table');
+    field.remove();
     this.displayOwnField();
   }
 
   updateEnemyField() {
-    const container1 = this.root.querySelector('#player2-container');
-    this.container1.remove();
+    const field = this.root.querySelector('#player2-container table');
+    field.remove();
     this.displayEnemyField();
   }
 }
 
-// Shoot functions
-
-const playerShoot = (x, y) => {
-  const result = getHitMiss(x, y, botField);
-  botField[x][y] = result === 'H' ? 'H' : 'M';
-  const cell = getElementByCoords(x, y, container2);
-  cell.innerText = result === 'H' ? 'H' : 'M';
-  cell.classList.add(result === 'H' ? 'hit' : 'miss');
-  const event = new Event('playerShoot');
-  document.dispatchEvent(event);
-};
-
-const botShoot = () => {
-  let x, y;
-  do {
-    x = Math.floor(Math.random() * size);
-    y = Math.floor(Math.random() * size);
-  } while (ownField[x][y] === 'M' || ownField[x][y] === 'H');
-
-  const result = getHitMiss(x, y, ownField);
-  ownField[x][y] = result === 'H' ? 'H' : 'M';
-  const cell = getElementByCoords(x, y, container1);
-  cell.innerText = result === 'H' ? 'H' : 'M';
-  cell.classList.add(result === 'H' ? 'hit' : 'miss');
-  const event = new Event('botShoot');
-  document.dispatchEvent(event);
-};
-
-const checkFieldForShips = (field) => {
-  return field.flat().some((cell) => cell === 'S');
-};
-
-const getElementByCoords = (x, y, root) => {
-  const rows = root.querySelectorAll('tr');
-  const cells = rows[y].querySelectorAll('td');
-  return cells[x];
-};
-
-const getHitMiss = (x, y, field) => {
-  if (field[x][y] === 'S') {
-    return 'H';
-  } else {
-    return 'M';
+class Model {
+  constructor() {
+    this.ownField = Array(size)
+      .fill(null)
+      .map(() => Array(size).fill('E'));
+    this.enemyField = Array(size)
+      .fill(null)
+      .map(() => Array(size).fill('?'));
   }
+
+  checkCell(x, y) {
+    if (this.ownField[y][x] === 'S') {
+      return 'H';
+    } else {
+      return 'M';
+    }
+  }
+
+  checkField() {
+    return this.ownField.flat().some((cell) => cell === 'S');
+  }
+
+  updateCell(x, y, value, fieldMark) {
+    if (fieldMark === 'own') {
+      this.ownField[y][x] = value;
+    } else if (fieldMark === 'enemy') {
+      this.enemyField[y][x] = value;
+    }
+  }
+}
+
+class Controller {
+  constructor(playerName, model, view) {
+    this.model = model;
+    this.playerName = playerName;
+    if (view) {
+      this.view = view;
+    }
+  }
+
+  shoot(x, y) {
+    const event = new CustomEvent('shot', {
+      detail: {
+        playerName: this.playerName,
+        coords: { x, y },
+      },
+    });
+    document.dispatchEvent(event);
+  }
+
+  receiveShoot(x, y) {
+    const result = this.model.checkCell(x, y);
+    this.model.updateCell(x, y, result, 'own');
+    if (this.view) {
+      //   this.view.updateOwnField();
+    }
+    const event = new CustomEvent('shotResult', {
+      detail: {
+        playerName: this.playerName,
+        coords: { x, y },
+        result,
+      },
+    });
+    document.dispatchEvent(event);
+  }
+
+  processShotFeedback(x, y, result) {
+    this.model.updateCell(x, y, result, 'enemy');
+    if (this.view) {
+      //   this.view.updateEnemyField();
+    }
+    if (!this.model.checkField()) {
+      const event = new CustomEvent('gameComplete', {
+        detail: {
+          playerName: this.playerName,
+        },
+      });
+      document.dispatchEvent(event);
+    }
+  }
+}
+
+const botShoot = (model) => {
+  const processEnemyField = (field) => {
+    return field.flat().reduce((acc, cur, index) => {
+      if (!acc.hasOwnProperty(cur)) {
+        acc[cur] = [];
+      }
+      acc[cur].push({ x: index % size, y: Math.floor(index / size) });
+      return acc;
+    }, {});
+  };
+  const enemyField = processEnemyField(model.enemyField);
+  const potentialTargets = enemyField['?'];
+  return potentialTargets[Math.floor(Math.random() * potentialTargets.length)];
 };
 
 const showVictory = (winner, gameField, victoryContainer) => {
   gameField.classList.add('hide');
   victoryContainer.classList.remove('hide');
-  victoryContainer.innerText = `${winner} Wins!`;
+  victoryContainer.innerText = `${winner} Loses!`;
 };
 
 const gameField = document.querySelector('#game-field');
@@ -119,44 +170,51 @@ const container1 = document.querySelector('#player1-container');
 const container2 = document.querySelector('#player2-container');
 const victoryContainer = document.querySelector('#victory');
 
-const ownField = Array(size)
-  .fill(null)
-  .map(() => Array(size).fill('E'));
-const botField = Array(size)
-  .fill(null)
-  .map(() => Array(size).fill('E'));
-
-// Place ships
-
-ownField[1][1] = 'S';
-ownField[0][0] = 'S';
-botField[2][2] = 'S';
-botField[0][2] = 'S';
-
 const game = () => {
-  const playerModel = {
-    ownField: ownField,
-    enemyField: Array(size)
-      .fill(null)
-      .map(() => Array(size).fill('?')),
-  };
-  const view = new View(playerModel, gameField, playerShoot);
+  const playerModel = new Model();
+  const playerController = new Controller('player', playerModel);
+  const view = new View(playerModel, gameField, playerController);
+
+  const botModel = new Model();
+  const botController = new Controller('bot', botModel);
+
+  // Place ships
+
+  playerModel.ownField[1][1] = 'S';
+  playerModel.ownField[0][0] = 'S';
+  botModel.ownField[2][2] = 'S';
+  botModel.ownField[0][2] = 'S';
+
   view.displayAll();
-  document.addEventListener('playerShoot', () => {
-    botShoot();
-  });
-  document.addEventListener('botShoot', () => {
-    if (checkFieldForShips(ownField) && checkFieldForShips(botField)) {
-      console.log('next round');
+  document.addEventListener('shot', (event) => {
+    const { playerName, coords } = event.detail;
+    const { x, y } = coords;
+    if (playerName === 'bot') {
+      playerController.receiveShoot(x, y);
     } else {
-      let winner;
-      if (checkFieldForShips(ownField)) {
-        winner = 'Player';
-      } else {
-        winner = 'Bot';
-      }
-      showVictory(winner, gameField, victoryContainer);
+      botController.receiveShoot(x, y);
     }
+  });
+  document.addEventListener('shotResult', (event) => {
+    const { playerName, coords, result } = event.detail;
+    const { x, y } = coords;
+    if (playerName === 'bot') {
+      playerController.processShotFeedback(x, y, result);
+      const { x: botShotGuessX, y: botShotGuessY } = botShoot(botModel);
+      console.log('bot shot x', botShotGuessX, 'y', botShotGuessY);
+      botController.shoot(botShotGuessX, botShotGuessY);
+      view.updateOwnField();
+      view.updateEnemyField();
+    } else {
+      botController.processShotFeedback(x, y, result);
+      view.updateOwnField();
+      view.updateEnemyField();
+    }
+  });
+  document.addEventListener('gameComplete', (event) => {
+    console.log(event);
+    const { playerName } = event.detail;
+    showVictory(playerName, gameField, victoryContainer);
   });
 };
 
