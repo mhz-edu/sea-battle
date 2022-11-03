a482a7cac24f694aad20ff8c456d2247d378c5f9
unknown <mhz.edu@gmail.com>
2022-11-03 12:02:31 +0200

    Further menu state refactor

--- states/menu/MenuState.js
+++ states/menu/MenuState.js
@@ -5,24 +5,22 @@ class MenuState extends BaseState {
         <div>
           <my-text text="Main menu"></my-text>
         </div>
-        <my-list>
-          <li slot="item"><my-button title="Start Game with Bot" data-value="newGame" click="processUserSelect"></my-button></li>
-          <li slot="item"><my-button title="Multiplayer - Start" data-value="multiStart" click="processUserSelect"></my-button></li>
-          <li slot="item"><my-button title="Multiplayer - Connect" data-value="multiConn" click="processUserSelect"></my-button></li>
+        <my-list click="processUserSelect">
+          <li slot="item"><my-button title="Start Game with Bot" data-value="newGame"></my-button></li>
+          <li slot="item"><my-button title="Multiplayer - Start" data-value="multiStart"></my-button></li>
+          <li slot="item"><my-button title="Multiplayer - Connect" data-value="multiConn"></my-button></li>
         </my-list>
       <div>
       `);
   }
 
   processUserSelect(clickEvent) {
-    clickEvent.stopImmediatePropagation();
+    const options = {
+      newGame: { userRole: 'main', gameType: 'single' },
+      multiStart: { userRole: 'main', gameType: 'multi' },
+      multiConn: { userRole: 'second', gameType: 'multi' },
+    };
     const userSelect = clickEvent.target.dataset.value;
-    if (userSelect === 'newGame') {
-      stateMachine.change('ships', { userRole: 'main', gameType: 'single' });
-    } else if (userSelect === 'multiStart') {
-      stateMachine.change('ships', { userRole: 'main', gameType: 'multi' });
-    } else if (userSelect === 'multiConn') {
-      stateMachine.change('ships', { userRole: 'second', gameType: 'multi' });
-    }
+    stateMachine.change('ships', options[userSelect]);
   }
 }
