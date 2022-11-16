9ced17e5c464face32ec1c64cac8251f28406ffe
unknown <mhz.edu@gmail.com>
2022-11-16 23:15:03 +0200

    Added random ship placement to Model

--- states/game/Model.js
+++ states/game/Model.js
@@ -140,6 +140,7 @@ class Model extends Subscribable {
         this.updateCell(colIndex, rowIndex, 'E', 'own');
       });
     });
+    this.resetMask();
   }
 
   placeShip(x, y, shipSize, shipOrient) {
@@ -153,6 +154,7 @@ class Model extends Subscribable {
         y = y + 1;
       }
     }
+    this.resetMask();
   }
 
   getMask(shipSize, shipOrient) {
@@ -174,5 +176,69 @@ class Model extends Subscribable {
     }
 
     return this._mask;
+  }
+
+  resetMask() {
+    this._mask = Array(size)
+      .fill(null)
+      .map(() => Array(size).fill(true));
+  }
+
+  placeShipRandom(shipSize) {
+    const orientation = Math.random() >= 0.5 ? 'h' : 'v';
+
+    const mask = this.getMask(shipSize, orientation);
+
+    let tries = size;
+    let trySuccessful = false;
+    let result = [];
+    while (!trySuccessful && tries > 0) {
+      const randomRowColumn = Math.floor(Math.random() * (size - shipSize));
+
+      const emptyCellsMap = utils.getEmptyCellsInRow(mask[randomRowColumn]);
+
+      for (let { start, end, length } of emptyCellsMap) {
+        if (shipSize <= length) {
+          // select random placement inside free space
+          const randomStart = Math.floor(
+            Math.random() * (length - shipSize + 1)
+          );
+          // place ship
+          this.placeShip(
+            start + randomStart,
+            randomRowColumn,
+            shipSize,
+            orientation
+          );
+          trySuccessful = true;
+          result.push(
+            start + randomStart,
+            randomRowColumn,
+            shipSize,
+            orientation
+          );
+          break;
+        }
+      }
+      tries = tries - 1;
+    }
+    if (trySuccessful) {
+      return result;
+    } else {
+      throw new Error(`Cannot place ship ${shipSize}`);
+    }
+  }
+
+  randomShipsFill(ships) {
+    while (!Object.values(ships).every((val) => val === 0)) {
+      const biggestShipSize = Math.max(
+        ...Object.entries(ships)
+          .filter(([size, qty]) => qty !== 0)
+          .map(([size, qty]) => size)
+      );
+
+      this.placeShipRandom(biggestShipSize);
+      ships[biggestShipSize] = ships[biggestShipSize] - 1;
+    }
   }
 }
