10f9243245da2194829ad73d1fcf87ac43403662
unknown <mhz.edu@gmail.com>
2022-11-26 17:52:20 +0200

    Reworked random ship placement to be more robust

--- states/game/Model.js
+++ states/game/Model.js
@@ -59,6 +59,7 @@ class Model extends Subscribable {
   }
 
   placeShip(x, y, shipSize, shipOrient) {
+    // console.log(...arguments);
     let delta = shipSize;
     while (delta > 0) {
       this.updateCell(x, y, 'S', 'own');
@@ -99,65 +100,78 @@ class Model extends Subscribable {
       .map(() => Array(size).fill(true));
   }
 
-  placeShipRandom(shipSize) {
-    /*Alghoritm description
-     *   1. select random orientation
-     *   2. update mask array -> mark ship cells and cells around ship as blocked for placement
-     * While there are tries and ship is not placed yet:
-     *   3. select random number which will be used as row or column number
-     *   4. break selected row or column into parts available for placement
-     *   5. iterating over parts, select first which length is less or equal to ship size
-     *   6. place ship with random offset inside selcted part, deduct ship from available and break iterating
-     *   7. if placement was successful, stop cycle, else try to place same ship again in another row or column
-     * If number of tries to place the ship exceeds 10, throw an error
-     */
-    const orientation = Math.random() >= 0.5 ? 'h' : 'v';
+  getPossibleplacements(shipSize) {
+    const orientations = [
+      { orientation: 'h', rowCol: 'rows' },
+      { orientation: 'v', rowCol: 'cols' },
+    ];
+    const result = [];
 
-    const mask = this.getMask(shipSize, orientation);
-
-    let tries = size;
-    let trySuccessful = false;
-    while (!trySuccessful && tries > 0) {
-      const randomRowColumn = Math.floor(Math.random() * (size - shipSize));
-
-      const emptyCellsMap = utils.getEmptyCellsInRow(mask[randomRowColumn]);
-
-      for (let { start, end, length } of emptyCellsMap) {
-        if (shipSize <= length) {
-          // select random placement inside free space
-          const randomStart = Math.floor(
-            Math.random() * (length - shipSize + 1)
-          );
-          // place ship
-          this.placeShip(
-            start + randomStart,
-            randomRowColumn,
-            shipSize,
-            orientation
-          );
-          trySuccessful = true;
-          break;
-        }
-      }
-      tries = tries - 1;
-    }
-    if (trySuccessful) {
-      return;
-    } else {
-      throw new Error(`Cannot place ship ${shipSize}`);
-    }
+    orientations.forEach(({ orientation, rowCol }) => {
+      const mask = this.getMask(shipSize, orientation);
+      [...this[rowCol]('_mask')].forEach((line, lineIndex) => {
+        const emptyCellsMap = utils.getEmptyCellsInRow(line);
+        emptyCellsMap.forEach(({ start, end, length }) => {
+          for (
+            let placeStart = start;
+            placeStart <= length - shipSize ||
+            (length === shipSize && placeStart === start);
+            placeStart++
+          ) {
+            if (orientation === 'h') {
+              result.push([placeStart, lineIndex, shipSize, orientation]);
+            } else {
+              result.push([lineIndex, placeStart, shipSize, orientation]);
+            }
+          }
+        });
+      });
+    });
+    return result;
   }
 
-  randomShipsFill(ships) {
-    while (!Object.values(ships).every((val) => val === 0)) {
-      const biggestShipSize = Math.max(
-        ...Object.entries(ships)
-          .filter(([size, qty]) => qty !== 0)
-          .map(([size, qty]) => size)
-      );
+  /*  Alghoritm description
+   *   Task of ship placement can be represented as tree DFS task, where
+   *    - edge is step of particular ship placement,
+   *    - node is array of all posible placements of particular ship on current field
+   */
 
-      this.placeShipRandom(biggestShipSize);
-      ships[biggestShipSize] = ships[biggestShipSize] - 1;
-    }
+  randomShipsFill(ships) {
+    const recurse = (ships, steps) => {
+      if (Object.values(ships).every((val) => val === 0)) {
+        return steps;
+      } else {
+        const biggestShipSize = Math.max(
+          ...Object.entries(ships)
+            .filter(([size, qty]) => qty !== 0)
+            .map(([size, qty]) => size)
+        );
+
+        const places = this.getPossibleplacements(biggestShipSize);
+
+        while (places.length > 0) {
+          const randomPlacementIndex = Math.floor(
+            Math.random() * places.length
+          );
+          const [randomPlacement] = places.splice(randomPlacementIndex, 1);
+          this.placeShip(...randomPlacement);
+
+          const result = recurse(
+            { ...ships, [biggestShipSize]: ships[biggestShipSize] - 1 },
+            [...steps, randomPlacement]
+          );
+          if (result.length >= steps.length + 1) {
+            return result;
+          }
+        }
+
+        steps.pop();
+        this.resetField();
+        steps.forEach((step) => this.placeShip(...step));
+        return steps;
+      }
+    };
+
+    return recurse(ships, []);
   }
 }
