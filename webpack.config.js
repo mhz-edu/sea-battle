bb2368471efc4090361ddb9a6d7252695044a70a
unknown <mhz.edu@gmail.com>
2022-11-29 00:31:57 +0200

    Updated config to use styles and html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   mode: "development",
-  entry: ["./src/Components.js", "./src/index.js"],
+  entry: ["./src/assets/style.scss", "./src/Components.js", "./src/index.js"],
   devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/assets/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
